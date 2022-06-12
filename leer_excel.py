75944cf16d19b59761cdbd36574ebe1be913657d
VicenteSK <valenzuelacortes@gmail.com>
2022-06-11 20:13:49 -0400

    ingreso validaciones 3 y 4 ok

--- leer_excel.py
+++ leer_excel.py
@@ -11,29 +11,51 @@ def estaVacio(df):
         print("no esta vacio")
         return False
 
-def esExcel(df):
-    #... algo que valide que sea un excel
+def esExcel(file):
+    if ".xls" in file:
+        print("si es archivo excel")
+        return True
+    else:
+        print("no es archivo excel")
+        return False
+        
+
 
 def checkColumnas(df):
-    #..
-
-
+    columnas = ["Nombre", "ApellidoPaterno", "ApellidoMaterno", "Sexo", "Edad", "Run", "Parentesco"]
+    for columna in columnas:
+        if columna in df:
+            print("si está esta columna " + columna)
+        else:
+            print("no se encuentra columna " + columna)
+            return False
+    return True
 
 @app.route('/data', methods =['GET', 'POST'])
 def data():
     if request.method == 'GET':
         # file = request.form['upload-file']
-        file = "integrantesfamiliareceptora.xlsx"
-        # file = "integrantesvacio.xlsx"
-        data = pd.read_excel(file)
+        file1 = "integrantesfamiliareceptora.xlsx"
+        file2 = "integrantesvacio.xlsx"
+        file3 = "integrantes4columnas.xlsx"
+        file4 = "integrantessinapellidomaterno.xlsx"
+        file5 = "pruebaword.docx"
+        file6 = "hola.xls.docx"
+
+        esExcel(file6)
+        data = pd.read_excel(file4)
+        #convierte excel en dataframe
 
         # 1º validacion: que no este vacio
+
         estaVacio(data)
+        
+        checkColumnas(data)
 
        # VALIDACIONE 1 que llegue el archivo
         # VALIDACIONE 2.que sea Excel
-        # VALIDACIONE 3.que no venga vacio ******
-        # VALIDACIONE 4 que vengan todas las columnas (en este caso el ejemplo es 7)
+        # VALIDACIONE 3.que no venga vacio *ok*****
+        # VALIDACIONE 4 que vengan todas las columnas (en este caso el ejemplo es 7) **OK
         # VALIDACIONE 15. que los datos de cada columna respeten el contrato (ejemplo nombre, que no tenga números, email, rut)
         ###
     
